07c8e0e39fd7c59c90a727a7446c321131257890
firahpp <firahputripratiwi10>
2025-11-03 21:37:09 +0700

    day 2 - increment

--- src/main/java/com/juaracoding/labs/App.java
+++ src/main/java/com/juaracoding/labs/App.java
@@ -1,24 +1,45 @@
 package com.juaracoding.labs;
 
 
-public class App 
-{
+public class App {
     public static void main(String[] args) {
-        // int umur = 10; //varibel sekaligus isi nilai
-        
-        // int nilai; //buat variable
-        // nilai = 90; //isi variabel
+      /**
+       * Implicit casting (casting otomatis)
+       */
+      // int i = 124_000_000; // kapasitas int di i, ukurannya lebih kecil dari kapasitas long di l.
+      // long l = i;
 
-        // int hasil = 7 * 4; //7*4 disini adalah ekspresi
-        // System.out.println(umur);
-        // System.out.println(nilai);
-        // System.out.println(hasil);
-        
-        int y = 10;
-        System.out.println(y);
-        y = y + 5;
-        System.out.println(y);
-        
-        
+      // System.out.println(i);
+      // System.out.println(l);
+
+      // /**
+      //  * Explicit casting
+      //  */
+      // short s = 100;
+      // byte b = (byte) s;
+      // System.out.println(s);
+      // System.out.println(b);
+
+      // int num1 = 10;
+      // int num2 = 20;
+      // double num3 = num1 / (double) num2;
+      // System.out.println(num3);
+
+      // int num4 = num1%3;
+      // System.out.println(num4);
+
+      // increment
+      // int x = 10;
+      // System.out.println(x++); // Pakai Dulu x nya, Baru Tambah x nya dengan 1
+
+      // int c = 1;
+      // System.out.println(++c); // Tambah dulu c nya, baru pakai (cetak)
+      // System.out.println(x);
+
+      int total = 140_000;
+      double discount = (double) 10/100;
+      double hargaPotongan = total * discount;
+      double hargaBayar = total - hargaPotongan;
+      System.out.println(hargaBayar);
     }
 }
