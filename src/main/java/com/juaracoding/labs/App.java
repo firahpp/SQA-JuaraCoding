9d28e2a83986f7eb7ce48b208e1b97a742612316
firahpp <firahputripratiwi10>
2025-11-04 22:04:13 +0700

    day 3 - switch

--- src/main/java/com/juaracoding/labs/App.java
+++ src/main/java/com/juaracoding/labs/App.java
@@ -6,64 +6,37 @@ import java.util.Scanner;
 public class App {
 
   public static void main(String[] args) throws Exception {
-    Scanner scanner = new Scanner(System.in);
+    int day = 3;
 
-    
-    int harga = 10000;
-    int uang = 5000;
-
-    if (uang >= harga) {
-      System.out.println("Saya bisa beli produk tersebut");
-    } else {
-      System.out.println("Saya tidak bisa beli produk tersebut");
+    switch (day) {
+      case 1:
+          System.out.println("Sunday");
+          break;
+      case 2:
+          System.out.println("Monday");
+          break;
+      case 3:
+          System.out.println("Tuesday");
+          break;
+      case 4:
+          System.out.println("Wednesday");
+          break;
+      case 5:
+          System.out.println("Thursday");
+          break;
+      case 6:
+          System.out.println("Friday");
+          break;
+      case 7:
+          System.out.println("Saturday");
+          break;
+      default:
+          System.out.println("Invalid day");
     }
 
-    System.out.println("Ini perintah berikutnya.");
-
-    String validUsername = "uus";
-    String validPassword = "123qwe";
-
-    String usernameDariUser = "oncom";
-    String passwordDariUser = "123qwa";
-
-    if (usernameDariUser.equals(validUsername)) {
-      if (passwordDariUser.equals(validPassword)) {
-        System.out.println("Login berhail");
-      } else {
-        System.out.println("Maaf password tidak valid.");
-      }
-    } else {
-      System.out.println("Maaf username tidak valid.");
-    }
     
-    try {
-      System.out.print("Masukkan nilai peserta: ");
-      int nilaiPeserta = scanner.nextInt();
-
-      if (nilaiPeserta == 0) {
-        System.out.println("Nilai peserta tidak boleh kosong");
-      } else {
-        int nilaiA = 90;
-        int nilaiB = 83;
-        int nilaiC = 70;
-        int nilaiD = 60;
-        
-        if (nilaiPeserta >= nilaiA) {
-          System.out.println("Dapet A");
-        } else if (nilaiPeserta >= nilaiB) {
-          System.out.println("Dapet B");
-        } else if (nilaiPeserta >= nilaiC) {
-          System.out.println("Dapet C");
-        } else if (nilaiPeserta >= nilaiD) {
-          System.out.println("Dapet D");
-        } else {
-          System.out.println("Dapet E");
-        }
-      }
-    } catch (InputMismatchException e) { //untuk menangkap kalau ada penulisan yang tidak sesuai
-      System.out.println("Ups terjadi masalah!");
-      System.out.println(e.getMessage());
-    }
+  
     
+
   }
 }
