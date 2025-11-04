3ec93ba6f37e488309d89f7c09bb78d07156620b
firahpp <firahputripratiwi10>
2025-11-04 22:08:48 +0700

    day 3 - game Kertas Gunting Batu

--- src/main/java/com/juaracoding/labs/App.java
+++ src/main/java/com/juaracoding/labs/App.java
@@ -1,37 +1,40 @@
 package com.juaracoding.labs;
 
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class App {
   public static void main(String[] args) throws Exception {
-    int i = 1;
-    while (i <= 5) {
-      System.out.println(i);
-      i++;
-    }
+    Scanner scanner = new Scanner(System.in);
+// --- Setup Variabel ---
+        int KERTAS = 0;
+        int GUNTING = 1;
+        int BATU = 2;
 
-    do {
-      System.out.println("Ini dari do while, jalan dulu, dicek belakangan!");
-      i++;
-    } while(i <= 5);
+        System.out.print("Masukkan angka: (0) Kertas, (1) Gunting, (2) Batu \t");
+        int playerChoice = scanner.nextInt(); // Input dari Anda (GUNTING)
 
-    int y = 0;
-    while (true) {
-      System.out.println(y);
-      y++;
-      if (y > 5) {
-        break;
-      }
+        // Kita acak pilihan komputer (0, 1, atau 2)
+        int computerChoice = ThreadLocalRandom.current().nextInt(0, 3);
+        
+        // Konversi angka ke teks agar mudah dibaca
+        String[] pilihanTeks = {"Kertas", "Gunting", "Batu"};
 
-    }
+        System.out.println("Player memilih: " + pilihanTeks[playerChoice]);
+        System.out.println("Komputer memilih: " + pilihanTeks[computerChoice]);
+        System.out.println("---");
 
-    int x = 0;
+if (playerChoice == computerChoice) {
+            System.out.println("Hasil 2: SERI!");
+        } 
+        else if ( playerChoice == (computerChoice + 1) % 3 ) {
+            System.out.println("Hasil 2: Player MENANG!");
+        } 
+        else {
+            System.out.println("Hasil 2: Player KALAH!");
+        }
 
-    while (x <= 10) {
-      x++;
-      if (x % 2 != 0) { //angka genap yg habis dibagi 2 maka akan dicetak
-        continue; //kalo angka ganjil maka tidak di cetak
-      }
-      System.out.println(x);
-    }
+
   }
 }
- 
+  
