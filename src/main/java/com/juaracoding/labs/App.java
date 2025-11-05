e1b9a9c4301efe1714c005a93d5a19a94fdac7bb
firahpp <firahputripratiwi10>
2025-11-05 19:25:01 +0700

    day 4 - array part 1

--- src/main/java/com/juaracoding/labs/App.java
+++ src/main/java/com/juaracoding/labs/App.java
@@ -1,39 +1,25 @@
 package com.juaracoding.labs;
 
-import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
-
 public class App {
   public static void main(String[] args) throws Exception {
-    Scanner scanner = new Scanner(System.in);
-// --- Setup Variabel ---
-        int KERTAS = 0;
-        int GUNTING = 1;
-        int BATU = 2;
+    int[] nilaiPeserta = {1, 4, 6, 8, 1};
+    //                    0  1  2  3  4
+    System.out.println(nilaiPeserta[0]);
+    // System.out.println(nilaiPeserta[6]); // error: diluar jangkauan
 
-        System.out.print("Masukkan angka: (0) Kertas, (1) Gunting, (2) Batu \t");
-        int playerChoice = scanner.nextInt(); // Input dari Anda (GUNTING)
+    int[] komplekDinas = new int[5];
+    komplekDinas[0] = 3;
+    komplekDinas[1] = 2;
+    komplekDinas[2] = 10;
+    komplekDinas[3] = 1;
+    komplekDinas[4] = 15;
+    
+    System.out.println(komplekDinas[2]);
 
-        // Kita acak pilihan komputer (0, 1, atau 2)
-        int computerChoice = ThreadLocalRandom.current().nextInt(0, 3);
-        
-        // Konversi angka ke teks agar mudah dibaca
-        String[] pilihanTeks = {"Kertas", "Gunting", "Batu"};
-
-        System.out.println("Player memilih: " + pilihanTeks[playerChoice]);
-        System.out.println("Komputer memilih: " + pilihanTeks[computerChoice]);
-        System.out.println("---");
-
-if (playerChoice == computerChoice) {
-            System.out.println("Hasil 2: SERI!");
-        } 
-        else if ( playerChoice == (computerChoice + 1) % 3 ) {
-            System.out.println("Hasil 2: Player MENANG!");
-        } 
-        else {
-            System.out.println("Hasil 2: Player KALAH!");
-        }
-
+    System.out.println("Perulangan dengan array");
+    for (int i = 0; i < komplekDinas.length; i++) {
+      System.out.println(komplekDinas[i]);
+    }
 
   }
 }
