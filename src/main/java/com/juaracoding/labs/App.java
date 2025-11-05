f8ffebdb967942e073375305fa5ffc4baeca4c07
firahpp <firahputripratiwi10>
2025-11-05 20:08:48 +0700

    day 4 - array part 2 (iterasi)

--- src/main/java/com/juaracoding/labs/App.java
+++ src/main/java/com/juaracoding/labs/App.java
@@ -2,25 +2,27 @@ package com.juaracoding.labs;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    int[] nilaiPeserta = {1, 4, 6, 8, 1};
-    //                    0  1  2  3  4
-    System.out.println(nilaiPeserta[0]);
-    // System.out.println(nilaiPeserta[6]); // error: diluar jangkauan
+    int[] subtotal = {
+      30000,
+      15000,
+      12000,
+      50500,
+      123000
+    };
+    int total = 0;
 
-    int[] komplekDinas = new int[5];
-    komplekDinas[0] = 3;
-    komplekDinas[1] = 2;
-    komplekDinas[2] = 10;
-    komplekDinas[3] = 1;
-    komplekDinas[4] = 15;
-    
-    System.out.println(komplekDinas[2]);
+    //perulangan iterasi item array versi old
+    // for (int i = 0; i < subtotal.length; i++){
+    //   total = total + subtotal[i];
+    // }
 
-    System.out.println("Perulangan dengan array");
-    for (int i = 0; i < komplekDinas.length; i++) {
-      System.out.println(komplekDinas[i]);
+    //iterasi item array modern
+
+    for (int harga: subtotal){
+      total = total + harga;
     }
 
+    System.out.println("Total: " + total);
   }
 }
   
