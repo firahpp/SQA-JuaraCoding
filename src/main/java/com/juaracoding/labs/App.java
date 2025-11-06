d7950e58dee94db79a48208bd823e70ea455ea3c
firahpp <firahputripratiwi10>
2025-11-06 21:38:18 +0700

    day 5 - mini quiz getter setter

--- src/main/java/com/juaracoding/labs/App.java
+++ src/main/java/com/juaracoding/labs/App.java
@@ -7,61 +7,21 @@ import com.juaracoding.labs.entity.Barang;
 import com.juaracoding.labs.entity.Customer;
 import com.juaracoding.labs.entity.DetailPesanan;
 import com.juaracoding.labs.entity.Pesanan;
+import com.juaracoding.labs.entity.userTest;
 
 public class App {
   public static void main(String[] args) throws Exception {
+
+    userTest userTest = new userTest();
+    userTest.setName("Jono");
+    userTest.setAlamat("Cibatu");
+    userTest.setNomorHP("0799345566");
    
-     Barang barang = new Barang();
-
-    // set data barang dari objek "barang"
-    barang.setKodeBarang("BRG001");
-    barang.setNama("Barang 01");
-    barang.setHarga(15000);
-    barang.setStok(100);
-    
-   Barang barang002 = new Barang();
-   barang002.setKodeBarang("BRG002");
-   barang002.setNama("Barang 02");
-   barang002.setHarga(25000);
-   barang002.setStok(10);
-
-   // Buat customer
-    Customer customer = new Customer();
-    customer.setKodeCustomer("CST001");
-    customer.setNamaCustomer("Bpk. Sarifudin");
-    customer.setNomorHPCustomer("08976654421");
-    customer.setAlamatCustomer("Jakarta Selatan");
-
-    //customer melakukan pesanan 
-    Pesanan pesanan001 = new Pesanan();
-    pesanan001.setKodePesan("PSN");
-    pesanan001.setCustomer(customer);
-    
-    //detail pesanan
-    DetailPesanan detailPesananBarang01 = new DetailPesanan();
-    detailPesananBarang01.setBarang(barang);
-    detailPesananBarang01.setPesanan(pesanan001);
-
-    DetailPesanan detailPesananBarang02 = new DetailPesanan();
-    detailPesananBarang02.setBarang(barang);
-    detailPesananBarang02.setPesanan(pesanan001);
-
-    DetailPesanan[] listTransaksi = {
-      detailPesananBarang01,
-      detailPesananBarang02
-    };
-
-    //perhitungan total detail pesanan
-    for (DetailPesanan detailPesanan: listTransaksi) {
-      int hargaBarang = detailPesanan.getBarang().getHarga();
-      int qty = detailPesanan.getQty();
-      detailPesanan.setSubtotal(hargaBarang*qty);
-      detailPesanan.getPesanan().setTotal(detailPesanan.getSubtotal());
-    }
-
-    System.out.println("Customer: " + pesanan001.getCustomer().getNamaCustomer());
-    System.out.println("Total: " + pesanan001.getTotal());
   
+  System.out.println("Hai: "+ userTest.getName());
+  System.out.println("Alamat kamu di : "+ userTest.getAlamat());
+  System.out.println("Nomor HP kamu : " + userTest.getNomorHP() );
+
   }
 }
   
---
+++ src/main/java/com/juaracoding/labs/entity/userTest.java
@@ -0,0 +1,28 @@
+package com.juaracoding.labs.entity;
+
+public class userTest {
+    private String name;
+    private String alamat;
+    private String nomorHP;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAlamat() {
+        return alamat;
+    }
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    public String getNomorHP() {
+        return nomorHP;
+    }
+    public void setNomorHP(String nomorHP) {
+        this.nomorHP = nomorHP;
+    }
+
+    
+}
